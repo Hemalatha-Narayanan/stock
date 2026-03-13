4bedf2853b6759042ac34ac1d9ea7a704371872a
Hemalatha Narayanan <hemanarayanan88@gmail.com>
2026-03-14 00:27:39 +0530

    Update POC.py

--- POC.py
+++ POC.py
@@ -1,54 +1,50 @@
-import json
+import _json
 import datetime as dt
+import pandas as pd
 import requests
-import pandas as pd
-import pandas as pd
-# pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
-github_api_url = "https://api.github.com/repos/squareshift/stock_analysis/contents/"
-response = requests.get(github_api_url)
-# print(response)
-# print(response.status_code)
-b = response.json()
-# a = response.text
-# print(a)
-# print(b)
-csv_files = [file['download_url'] for file in b if file['name'].endswith('.csv')]
-
-csv_file = csv_files.pop()
-# print(csv_file)
-d = pd.read_csv(csv_file)
-# print(d)
-# print(type(d))
-dataframes=[]
-file_names=[]
+pd.set_option('display.max_rows', None)
+github_api_url="https://api.github.com/repos/squareshift/stock_analysis/contents/"
+response=requests.get(github_api_url)
+print(response)
+#print(response.text) #in string format unaligned cannot be iterable
+b=response.json()  # organised and can be iterable using index
+#print( "Json fromat:",b)
+csv_files=[f['download_url'] for f in b if f['name'].endswith('.csv')]
+#print("downloadable url:",len(csv_files),csv_files) #display total url:21 and link
+csv_file=csv_files.pop() #last url only contains Meta data
+#print("Fetch Last Url contains Meta data:",csv_file)
+d=pd.read_csv(csv_file) #contains only meta data
+#print(d)
+#print(type(d))
+dataframe=[]
+filename=[]
 for url in csv_files:
-    file_name = url.split("/")[-1].replace(".csv", "")
-    # print(file_name)
+    filenames=url.split("/")[-1].replace(".csv","")
+    #print("Filename:",filenames)
+    df=pd.read_csv(url)
+    #print("df:",df)
+    df['Symbol']=filenames
+    #print(df['Symbol'])
+    dataframe.append(df)
+    filename.append(filenames)
+    #print(filename)
+    #print(dataframe)
 
-    df = pd.read_csv(url)
-    # print(df)
-    df['Symbol'] = file_name
-    # print(df['Symbol'])
-    dataframes.append(df)
-    # print(dataframes)
-    file_names.append(file_name)
-
-combined_df = pd.concat(dataframes, ignore_index=True)
-# print(combined_df)
+combined_df = pd.concat(dataframe, ignore_index=True) #here align datas inside dataframe using [pd.concat] pandas used for data processing
+#print(combined_df)
 o_df = pd.merge(combined_df,d,on='Symbol',how='left')
-# print(o_df)
-result = o_df.groupby("Sector").agg({'open':'mean','close':'mean','high':'max','low':'min','volume':'mean'}).reset_index()
-# print(result)
-
-o_df["timestamp"] = pd.to_datetime(o_df["timestamp"])
-# print(o_df["timestamp"])
-filtered_df = o_df[(o_df['timestamp'] >= "2021-01-01") & (o_df['timestamp'] <= "2021-05-26")]
-result_time = filtered_df.groupby("Sector").agg({'open':'mean','close':'mean','high':'max','low':'min','volume':'mean'}).reset_index()
-# print(result_time)
-list_sector = ["TECHNOLOGY","FINANCE"]
-result_time = result_time[result_time["Sector"].isin(list_sector)].reset_index(drop=True)
-print(result_time)
-path = r"C:\Users\Deepika Mohan\OneDrive\Documents\stock_data.csv"
-result_time.to_csv(path, header=True)
-print('data saved sucessfully')
+#print(o_df)
+result=o_df.groupby("Sector").agg({'open':'mean','close':'mean','high':'max','low':'min','volume':'mean'}).reset_index()
+#print(result)
+o_df["timestamp"]=pd.to_datetime(o_df['timestamp'])
+#print(o_df)
+filtered_df=o_df[(o_df['timestamp'] >= "2021-01-01") & (o_df['timestamp'] <= "2021-05-26")]
+res_time=filtered_df.groupby("Sector").agg({'open':'mean','close':'mean','high':'max','low':'min','volume':'mean'}).reset_index()
+#print(res_time)
+list_sector=["TRADE & SERVICES","LIFE SCIENCES"]
+res_time = res_time[res_time["Sector"].isin(list_sector)].reset_index(drop=True)
+print(res_time)
+path=r"C:\Users\rd070\OneDrive\Desktop\Data Engineering\stock analysis.csv"
+res_time.to_csv(path,index=True)
+print("Poc Completed")
